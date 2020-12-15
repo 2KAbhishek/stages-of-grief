0609a9d5783b2f33fd9b109588aa76e5885d8b8b
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-12-16 00:38:06 +0530

    Add event listener to panels

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-// $=_=>`$=${$};$()`;$()
+let panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    makeInactive();
+    panel.classList.add("active");
+  });
+});
 
 console.log("Tiny Web");
